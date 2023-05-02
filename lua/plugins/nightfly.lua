fd16b321f21da242a1be310696d376afca9feabd
Siegfred Balona <siegblink@gmail.com>
2023-05-02 11:18:03 +0800

    📦 NEW: install nightfly theme

---
+++ lua/plugins/nightfly.lua
@@ -0,0 +1,11 @@
+return {
+  { "bluz71/vim-nightfly-colors" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nightfly",
+      lazy = false,
+      priority = 1000,
+    },
+  },
+}
